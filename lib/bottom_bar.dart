a94ec90ce7435967ad0e52cce825dc44d5e6e554
Sagar Mangroliya <sagarspatel750@gmail.com>
2023-03-10 10:07:45 +0530

    done

---
+++ lib/bottom_bar.dart
@@ -0,0 +1,160 @@
+// ignore_for_file: camel_case_types, sized_box_for_whitespace, prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class bottom_bar extends StatefulWidget {
+
+  bool isPlaying;
+  bottom_bar(this.isPlaying);
+
+  @override
+  State<bottom_bar> createState() => _bottom_barState();
+}
+
+class _bottom_barState extends State<bottom_bar> {
+  @override
+  Widget build(BuildContext context) {
+
+    bool isPlaying = widget.isPlaying;
+
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBody: true,
+      appBar:AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xff3D3C3C),
+        elevation: 0.0,
+        title:Text("Player",style:TextStyle(fontSize:33,fontFamily:"font",color:Colors.red.shade400)),
+        centerTitle:true,
+      ),
+      backgroundColor:  Color(0xff3D3C3C),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              SizedBox(height: 20),
+              Container(
+                  padding: EdgeInsets.all(53),
+                  decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(25)),
+                  child:
+                      Icon(Icons.music_note, size: 170, color: Colors.grey)),
+              Column(
+                children: [
+                  SizedBox(height: 27),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: SliderTheme(
+                        data: SliderThemeData(
+                            trackHeight: 1,
+                            thumbColor: Color(0xffEE2D2D),
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 6),
+                            activeTrackColor: Color(0xffEE2D2D),
+                            inactiveTrackColor: Colors.black87),
+                        child: Slider.adaptive(
+                          value: 35,
+                          onChanged: (value) {},
+                          min: 0,
+                          max: 100,
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 45),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("00:00",
+                            style:
+                                TextStyle(color: Colors.red, fontSize: 12)),
+                        Text("04:08",
+                            style: TextStyle(
+                                color: Colors.white60, fontSize: 12)),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
+              Container(
+                height: 40,
+                width: 270,
+                child: Text("Song Name track one Mp3",
+                    style: TextStyle(fontSize: 24, color: Colors.white)),
+              ),
+              Text("Unknown artists",
+                  style: TextStyle(color: Colors.white60, fontSize: 13)),
+              SizedBox(height: 50),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.skip_previous),
+                      iconSize: 40,
+                      color: Color(0xffEE2D2D),
+                    ),
+                    SizedBox(
+                        height: 60,
+                        width: 60,
+                        child: FloatingActionButton(
+                            backgroundColor: Color(0xffEE2D2D),
+                            onPressed: () {
+                              setState(() {
+                                if (isPlaying) {
+                                  isPlaying = false;
+                                } else {
+                                  isPlaying = true;
+                                }
+                              });
+                            },
+                            child: isPlaying
+                                ? Icon(Icons.play_arrow, size: 40,)
+                                : Icon(Icons.pause, size: 40,))),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.skip_next,
+                          color: Color(0xffEE2D2D),
+                          size: 40,
+                        ))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.arrow_back,
+                            color: Colors.white, size: 22)),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border,
+                            color: Colors.white, size: 22)),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shuffle_outlined,
+                            color: Colors.white, size: 22)),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.more_horiz,
+                            color: Colors.white, size: 22))
+                  ],
+                ),
+              )
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:music_player/Album_pg.dart';
 import 'package:music_player/Artists_pg.dart';
 import 'package:music_player/MusicPlayerList.dart';
 import 'package:music_player/Playlists_pg.dart';
+import 'package:music_player/bottom_bar.dart';
 
 void main() {
   runApp(MaterialApp(home: demo(), debugShowCheckedModeBanner: false));
@@ -18,6 +19,9 @@ class demo extends StatefulWidget {
 }
 
 class _demoState extends State<demo> {
+
+  static bool isPlaying = true;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -29,113 +33,9 @@ class _demoState extends State<demo> {
             // Bottom Bar/////
             InkWell(
               onTap: () {
-                showModalBottomSheet(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  backgroundColor: Color(0xff3D3C3C),
-                  isScrollControlled: true,
-                  barrierColor: Colors.black,
-                  context: context,
-                  builder: (context) {
-                    return Container(
-                      height: size.height - 60,
-                      child: Column(
-                        children: [
-                          Icon(Icons.drag_handle, color: Colors.grey.shade700),
-                          SizedBox(height: 20),
-                          Container(
-                              padding: EdgeInsets.all(53),
-                              decoration: BoxDecoration(
-                                  color: Colors.grey.withOpacity(0.1),
-                                  borderRadius: BorderRadius.circular(25)),
-                              child: Icon(Icons.music_note,
-                                  size: 170, color: Colors.grey)),
-                          Column(
-                            children: [
-                              SizedBox(height:27),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal:20),
-                                child: SliderTheme(
-                                    data: SliderThemeData(
-                                        trackHeight:1,
-                                        thumbColor:Color(0xffEE2D2D),
-                                        thumbShape:RoundSliderThumbShape(enabledThumbRadius:6),
-                                        activeTrackColor:Color(0xffEE2D2D),
-                                        inactiveTrackColor: Colors.black87
-                                    ),
-                                    child: Slider.adaptive(
-                                      value:35,
-                                      onChanged: (value) {},
-                                      min:0,max:100,
-                                    )),
-                              ),
-                              Padding(
-                                padding:EdgeInsets.symmetric(horizontal:45),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("00:00",style:TextStyle(color:Colors.red,fontSize:12)),
-                                    Text("04:08",style:TextStyle(color:Colors.white60,fontSize:12)),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(height:30),
-                            ],
-                          ),
-                          Container(
-                            height:40,width:270,
-                            child: Text("Song Name track one Mp3",style:TextStyle(fontSize:24,color: Colors.white)),
-                          ),
-                          Text("Unknown artists",style:TextStyle(color: Colors.white60,fontSize:13)),
-                          SizedBox(height:50),
-                          Padding(
-                            padding:  EdgeInsets.symmetric(horizontal:25),
-                            child: Row(
-                              mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-                              children: [
-                                IconButton(onPressed: () {
-
-                                }, icon: Icon(Icons.skip_previous),iconSize:40,color:Color(0xffEE2D2D),),
-                                SizedBox(
-                                  height:60,width:60,
-                                  child: FloatingActionButton  (
-                                    backgroundColor:Color(0xffEE2D2D),
-                                      onPressed: () {
-
-                                  },child: Icon(Icons.play_arrow_sharp,size:40,color:Colors.white,)),
-                                ),
-                                IconButton(
-                                  onPressed: () {
-
-                                }, icon: Icon(Icons.skip_next),iconSize:40,color:Color(0xffEE2D2D),)
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding:  EdgeInsets.only(top:40,left:20,right:20),
-                            child: Row(
-                              mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-                              children: [
-                                IconButton(onPressed: () {
-                                  Navigator.pop(context);
-                                }, icon:Icon(Icons.arrow_back,color:Colors.white,size:22)),
-                                IconButton(onPressed: () {
-
-                                }, icon:Icon(Icons.favorite_border,color:Colors.white,size:22)),
-                                IconButton(onPressed: () {
-
-                                }, icon:Icon(Icons.shuffle_outlined,color:Colors.white,size:22)),
-                                IconButton(onPressed: () {
-
-                                }, icon:Icon(Icons.more_horiz,color:Colors.white,size:22))
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                );
+                Navigator.push(context,MaterialPageRoute(builder: (context) {
+                  return bottom_bar(isPlaying);
+                },));
               },
               child: Container(
                 margin: EdgeInsets.only(bottom: 10),
@@ -176,9 +76,15 @@ class _demoState extends State<demo> {
                                   borderRadius: BorderRadius.circular(20),
                                   border: Border.all(color: Colors.white)),
                               child: IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.play_arrow_sharp,
-                                      size: 22, color: Colors.red)),
+                                  onPressed: () {
+                                 setState(() {
+                                  if(isPlaying) {isPlaying = false;}
+                                  else {isPlaying = true;}
+                                 });
+                                  },
+                                  icon: isPlaying
+                                      ? Icon(Icons.play_arrow_sharp, size: 22, color: Colors.red)
+                                      : Icon(Icons.pause, size: 22, color: Colors.red)),
                             ),
                             IconButton(
                                 onPressed: () {},
@@ -194,8 +100,19 @@ class _demoState extends State<demo> {
           backgroundColor: Colors.black,
           appBar: AppBar(
             backgroundColor: Colors.black,
-            title: Text("MUSIC 🎸",style:TextStyle(letterSpacing:1.5,fontFamily: "font",color:Colors.red,
-                fontSize:30,fontStyle:FontStyle.italic,shadows:[Shadow(color:Colors.redAccent,blurRadius:5,offset:Offset(-1.5,1))])),
+            title: Text("MUSIC 🎸",
+                style: TextStyle(
+                    letterSpacing: 1.5,
+                    fontFamily: "font",
+                    color: Colors.red,
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic,
+                    shadows: [
+                      Shadow(
+                          color: Colors.redAccent,
+                          blurRadius: 5,
+                          offset: Offset(-1.5, 1))
+                    ])),
             actions: [
               Icon(
                 Icons.search_rounded,
